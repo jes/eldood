5b52bfb30868386684095ea7d1b630b346bd47d2
James Stanley <james@incoherency.co.uk>
2022-06-30 14:08:46 +0100

    Draw convincing calendar

--- public/js/app.js
+++ public/js/app.js
@@ -10,6 +10,27 @@ function fmtDate(year, month, day) {
     return zeropad(year,4) + zeropad(month+1,2) + zeropad(day,2);
 }
 
+// https://stackoverflow.com/a/1184359
+function daysInMonth(month, year) {
+    return new Date(year, month, 0).getDate();
+}
+
+function dayOfWeek(month, year, day) {
+    let d = new Date();
+    d.setDate(day);
+    d.setMonth(month);
+    d.setYear(year);
+    return d.getDay();
+}
+
+// https://www.w3docs.com/snippets/javascript/how-to-create-a-new-dom-element-from-html-string.html
+function htmlToElem(html) {
+    let temp = document.createElement('template');
+    html = html.trim(); // Never return a space text node as a result
+    temp.innerHTML = html;
+    return temp.content.firstChild;
+}
+
 function makeDatesInput(container) {
     let now = new Date();
 
@@ -47,17 +68,14 @@ function makeDatesInput(container) {
             if (datesInput.selected[date]) dates.push(date);
         }
         datesInput.inputField.value = dates.sort().join(',');
+        datesInput.createInput();
     };
     datesInput.createInput = function() {
+        // delete existing divs
         if (datesInput.navDiv1) {
             container.removeChild(datesInput.navDiv1);
             container.removeChild(datesInput.daysDiv);
             container.removeChild(datesInput.navDiv2);
-        }
-
-        // https://stackoverflow.com/a/1184359
-        function daysInMonth (month, year) {
-            return new Date(year, month, 0).getDate();
         }
 
         function navDiv() {
@@ -82,7 +100,7 @@ function makeDatesInput(container) {
             d.setDate(1);
             d.setMonth(datesInput.month);
             d.setYear(datesInput.year);
-            monthName.textContent = datesInput.month + " " + d.toLocaleString("default", {month:"long", year:"numeric"});
+            monthName.textContent = d.toLocaleString("default", {month:"long", year:"numeric"});
 
             navDiv.appendChild(minusYear);
             navDiv.appendChild(minusMonth);
@@ -93,17 +111,41 @@ function makeDatesInput(container) {
             return navDiv;
         }
 
+        // create top & bottom navdivs
         datesInput.navDiv1 = navDiv();
         datesInput.navDiv2 = navDiv();
 
+        // create table with td for each day
         datesInput.daysDiv = document.createElement('div');
-        for (let i = 1; i <= daysInMonth(datesInput.month, datesInput.year); i++) {
-            let span = document.createElement('span');
-            span.onclick = function() { datesInput.toggleDay(i) };
-            span.textContent = i;
-            datesInput.daysDiv.appendChild(span);
+        let table = document.createElement('table');
+        table.classList.add('date-table');
+        table.appendChild(htmlToElem("<tr><th>M</th><th>T</th><th>W</th><th>T</th><th>F</th><th>S</th><th>S</th></tr>"));
+        let row = document.createElement('tr');
+        let weekday = dayOfWeek(datesInput.month, datesInput.year, 1);
+        let td = document.createElement('td');
+        td.colSpan = weekday-1;
+        row.appendChild(td);
+        for (let day = 1; day <= daysInMonth(datesInput.month, datesInput.year); day++) {
+            let td = document.createElement('td');
+            td.onclick = function() { datesInput.toggleDay(day) };
+            td.textContent = day;
+            let date = fmtDate(datesInput.year, datesInput.month, day);
+            td.classList.add('date-input');
+            if (datesInput.selected[date]) {
+                td.classList.add('date-selected');
+            } else {
+                td.classList.add('date-unselected');
+            }
+            row.appendChild(td);
+            if (weekday++%7 == 0) {
+                table.appendChild(row);
+                row = document.createElement('tr');
+            }
         }
+        table.appendChild(row);
+        datesInput.daysDiv.appendChild(table);
 
+        // add divs to the container
         datesInput.container.appendChild(datesInput.navDiv1);
         datesInput.container.appendChild(datesInput.daysDiv);
         datesInput.container.appendChild(datesInput.navDiv2);
